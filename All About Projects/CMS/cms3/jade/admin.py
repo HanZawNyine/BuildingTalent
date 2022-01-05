29d32885c57b1ca14d6d66e72242dea54b70293b
h4n24wnyin3 <hanzawnyine143@gmail.com>
2022-01-05 22:34:45 +0630

    cms 3

---
+++ All About Projects/CMS/cms3/jade/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from . models import *
+# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(PostImage)
+admin.site.register(Comment)
