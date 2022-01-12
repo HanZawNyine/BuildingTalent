bd72d41a6fce04ffe19595fcdf8febda004c3162
h4n24wnyin3 <hanzawnyine143@gmail.com>
2022-01-12 13:22:21 +0630

    js java cpp py csharp singleton

---
+++ Software design pattern/1-Singleton Pattern/Singleton-CSharp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_CSharp
+{
+    public class Singleton
+    {
+        static Singleton instance;
+
+        // Constructor is 'protected'
+
+        protected Singleton()
+        {
+        }
+
+        public static Singleton Instance()
+        {
+            // Uses lazy initialization.
+            // Note: this is not thread safe.
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+
+            return instance;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Constructor is protected -- cannot use new
+
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+
+            // Test for same instance
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Objects are the same instance");
+                Console.WriteLine(s1.GetHashCode());
+                Console.WriteLine(s2.GetHashCode());
+            }
+
+            // Wait for user
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
